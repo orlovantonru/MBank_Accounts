3427b7365bc63091cb9a19d9f4ae9cf3613c51e1
orlov_anton <orlov_anton@mail.ru>
2023-10-07 15:58:36 +0300

    Initial commit

---
+++ src/main/java/com/minibank/mbank_accounts/MBankAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.minibank.mbank_accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MBankAccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MBankAccountsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minibank/mbank_accounts/MBankAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minibank.mbank_accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MBankAccountsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
